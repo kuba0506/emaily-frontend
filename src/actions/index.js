86eaaced2a32e08495400ee20e083e41fd2c619d
Kuba <kuba0506@gmail.com>
2018-08-09 08:34:24 +0200

    Add payments

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,26 @@ import axios from 'axios';
 import * as actions from './types';
 
 export const fetchUser = () => async dispatch => {
-        try {
-            const res = await axios.get('/api/current_user');
-            dispatch({
-                type: actions.FETCH_USER,
-                payload: res.data
-            })
-        } catch(e) {
-            console.log('Error: ', e);
-        }
+    try {
+        const res = await axios.get('/api/current_user');
+        dispatch({
+            type: actions.FETCH_USER,
+            payload: res.data
+        })
+    } catch (e) {
+        console.log('Error: ', e);
+    }
+};
+
+export const handleToken = token => async dispatch => {
+    try {
+        const res = await axios.post('/api/stripe', token);
+
+        dispatch({
+            type: actions.FETCH_USER,
+            payload: res.data
+        });
+    } catch (e) {
+        console.log('Error: ', e);
+    }
 };
--- src/components/header.component.js
+++ src/components/header.component.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import Payments from './payments.component';
 
 class Header extends Component {
     renderLinks() {
@@ -13,9 +14,13 @@ class Header extends Component {
                 );
             default:
                 return (
-                    <div>
-                        <li><a href="/api/logout">Logout</a></li>
-                    </div>
+                    [
+                        <li key="1"><Payments /></li>,
+                        <li key="2" style={{ margin: '0 10px' }}>
+                            Credits: {this.props.auth.credits}
+                        </li>,
+                        <li key="3"><a href="/api/logout">Logout</a></li>
+                    ]
                 );
         }
     }
